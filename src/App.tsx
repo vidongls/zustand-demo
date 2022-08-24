61c99b39ec832aed8f11b1fb68b75a46da6a596b
DongVi <vidongls12345@gmail.com>
2022-08-24 13:31:44 +0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { ITodo, useTodoStore } from "./store/TodoStore";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { todos, addTodo, removeTodo, updateTodo, completeTodo } = useTodoStore();
+	const [input, setInput] = useState("");
+	const [itemUpdate, setItemUpdate] = useState<ITodo>({ id: "", description: "", completed: false });
+	const [validate, setValidate] = useState(false);
+	const [isUpdate, setIsUpdate] = useState(false);
+
+	const onChangeInput = (value: string) => {
+		setInput(value);
+		setValidate(false);
+	};
+
+	const handleAddTodo = () => {
+		if (!input) {
+			setValidate(true);
+			return;
+		}
+		addTodo(input);
+	};
+
+	const handleUpdateTodo = (todo: ITodo) => {
+		setIsUpdate(true);
+		setInput(todo.description);
+		setItemUpdate(todo);
+	};
+
+	const onUpdateTodo = () => {
+		updateTodo({
+			...itemUpdate,
+			description: input,
+		});
+	};
+
+	return (
+		<div className="w-screen h-screen p-6 flex justify-center">
+			<div className="text-center flex flex-col">
+				<h1 className="text-3xl font-medium text-green-700 mb-3">Todo List</h1>
+				<div className="flex">
+					<input
+						type="text"
+						className="hidden sm:flex items-center w-96 text-left space-x-3 px-4 h-10 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500 shadow-sm rounded-tl-lg rounded-bl-lg text-slate-400"
+						onChange={(e) => onChangeInput(e.target.value)}
+						value={input}
+					/>
+					{isUpdate ? (
+						<button
+							className="h-10 px-6 font-semibold bg-blue-500 text-white rounded-tr-lg rounded-br-lg hover:bg-blue-400"
+							onClick={onUpdateTodo}
+						>
+							Edit
+						</button>
+					) : (
+						<button
+							className="h-10 px-6 font-semibold bg-blue-500 text-white rounded-tr-lg rounded-br-lg hover:bg-blue-400"
+							onClick={handleAddTodo}
+						>
+							Add
+						</button>
+					)}
+				</div>
+				{validate && <p className="text-red-600">Input invalid!</p>}
+				<ul className="mt-4">
+					{todos?.map((todo) => (
+						<li className="bg-blue-50 rounded-lg p-3 font-medium text-gray-700 flex justify-between mb-3" key={todo.id}>
+							<div className="flex align-center">
+								<input type="checkbox" checked={todo.completed} onChange={() => completeTodo(todo.id)} />
+								<span className="ml-2">{todo.description}</span>
+							</div>
+							<div>
+								<span
+									className="mr-2 text-green-600 hover:text-green-500 cursor-pointer"
+									onClick={() => handleUpdateTodo(todo)}
+								>
+									Update
+								</span>
+								<span className="text-red-500 hover:text-red-400 cursor-pointer" onClick={() => removeTodo(todo.id)}>
+									Delete
+								</span>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;
+
---
+++ src/store/TodoStore.ts
@@ -0,0 +1,62 @@
+import create from "zustand";
+import { devtools, persist } from "zustand/middleware";
+
+export interface ITodo {
+	id: string;
+	description: string;
+	completed: boolean;
+}
+
+interface TodoState {
+	todos: ITodo[];
+	addTodo: (description: string) => void;
+	removeTodo: (id: string) => void;
+	updateTodo: (todo: ITodo) => void;
+	completeTodo: (id: string) => void;
+}
+
+export const useTodoStore = create<TodoState>()(
+	devtools(
+		persist(
+			(set) => ({
+				todos: [],
+				addTodo: (description) =>
+					set((state: TodoState) => ({
+						todos: [
+							...state.todos,
+							{
+								id: new Date().toISOString(),
+								description,
+								completed: false,
+							} as ITodo,
+						],
+					})),
+				removeTodo: (id) =>
+					set((state: TodoState) => ({
+						todos: state.todos.filter((todo) => todo.id !== id),
+					})),
+				updateTodo: (todo: ITodo) =>
+					set((state: TodoState) => {
+						console.log("todo", todo);
+						const newListTodos = state.todos.map((todoItem) =>
+							todoItem.id === todo.id
+								? { ...todoItem, description: todo.description, completed: todo.completed }
+								: todoItem
+						);
+						return {
+							todos: newListTodos,
+						};
+					}),
+				completeTodo: (id: string) =>
+					set((state) => ({
+						todos: state.todos.map((todoItem) =>
+							todoItem.id === id ? { ...todoItem, completed: !todoItem.completed } : todoItem
+						),
+					})),
+			}),
+			{
+				name: "todos-storage",
+			}
+		)
+	)
+);
